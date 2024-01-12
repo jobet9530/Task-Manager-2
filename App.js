02a2be352590ca91e40c802b4e08fdcb4df5ab90
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2024-01-12 14:26:11 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,47 +1,47 @@
-import React from "react";
-import { StyleSheet, SafeAreaView, Text } from "react-native";
+import React, { useState } from "react";
+import { useDatabase } from "./useDatabase";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  TextInput,
+  Button,
+} from "react-native";
+import { Database } from "sqlite";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.h1}>Hello CodeSandbox</Text>
-        <Text style={styles.h2}>
-          Start editing to see some magic happen, even on your mobile device!
-        </Text>
-        <br />
-        <br />
-        <Text style={styles.paragraph}>
-          Open Expo on your mobile device with scanning the QR code in the
-          application log under the start tab.
-        </Text>
-      </SafeAreaView>
-    );
+const App = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const database = userDatabase();
+
+  const handleLogin = async () => {
+    try{
+      const = await database.getUser(email, password);
+      console.log("Login successful:",);
+    }
+    catch(error){
+      console.log(error); 
+    }
   }
-}
+
+  return (
+   <SafeAreaView style={styles.container}>
+    <Text style={styles.heading}>Login</Text>
+
+   </SafeAreaView> 
+  )
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    backgroundColor: "#ecf0f1",
-    padding: 8,
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
-  paragraph: {
-    margin: 8,
-    fontSize: 16,
-    textAlign: "center",
-  },
-  h1: {
-    margin: 28,
-    fontSize: 36,
+  heading: {
+    fontSize: 24,
     fontWeight: "bold",
-    textAlign: "center",
-  },
-  h2: {
-    margin: 16,
-    fontSize: 28,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-});
+    marginBottom: 20, 
+  }
+})
